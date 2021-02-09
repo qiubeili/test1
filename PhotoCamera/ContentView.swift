cf3546c5580ed936234a873af68190c9107cd4fb
Qiubei Li <qli5025@gmail.com>
2021-02-09 09:30:09 -0800

    Initial Commit

---
+++ PhotoCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoCamera
+//
+//  Created by Qiubei Li on 2/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
