63a9209874218b50d96364ea124846ac8278afde
Qiubei Li <qli5025@gmail.com>
2021-02-14 17:34:55 -0800

    Added photo library and camera

--- PhotoCamera/ContentView.swift
+++ PhotoCamera/ContentView.swift
@@ -8,9 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showSheet: Bool = false
+    @State private var showImagePicker: Bool = false
+    @State private var sourceType: UIImagePickerController.SourceType = .camera
+    
+    @State private var image: UIImage?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            VStack {
+                Text("Image Captioning Generator")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                    .frame(height:100)
+                Image(uiImage: image ?? UIImage(named: "image")!)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 350, height: 350)
+                    
+                Spacer()
+                    .frame(height:100)
+                
+                HStack{
+                    Button("Choose Picture") {
+                        self.showSheet = true
+                    }.padding()
+                        .actionSheet(isPresented: $showSheet) {
+                            ActionSheet(title: Text("Select Photo"), message: Text("Choose"), buttons: [
+                                .default(Text("Photo Library")) {
+                                    self.showImagePicker = true
+                                    self.sourceType = .photoLibrary
+                                },
+                                .default(Text("Camera")) {
+                                    self.showImagePicker = true
+                                    self.sourceType = .camera
+                                },
+                                .cancel()
+                            ])
+                        }
+                    .border(/*@START_MENU_TOKEN@*/Color("Color")/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/Color("Color")/*@END_MENU_TOKEN@*/)
+                    
+                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Generate Captions")
+                    }
+                    .padding()
+                    .border(Color("Color"), width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/Color("Color")/*@END_MENU_TOKEN@*/)
+                }
+            }
+            
+        }.sheet(isPresented: $showImagePicker) {
+            ImagePicker(image: self.$image, isShown: self.$showImagePicker, sourceType: self.sourceType)
+        }
     }
 }
 
